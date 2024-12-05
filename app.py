1fc66a184d124997531106917d8764cc8f3a788a
Zanges <3868054+Zanges@users.noreply.github.com>
2024-12-05 17:44:18 +0100

    add the ability to update post

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ provider = JsonProvider("./data.json")
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', posts=provider.get_data())
+    return render_template("index.html", posts=provider.get_data())
 
 
-@app.route('/add', methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
         title = request.form.get("title")
@@ -25,15 +25,37 @@ def add():
             "content": content
         }
         provider.add_data(data)
-        return redirect(url_for('index'))
-    return render_template('add.html')
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
 
-@app.route('/delete/<int:post_id>')
+@app.route("/delete/<int:post_id>")
 def delete(post_id: int):
     provider.delete_data(post_id)
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
-if __name__ == '__main__':
+@app.route("/update/<int:post_id>", methods=["GET", "POST"])
+def update(post_id: int):
+    post = provider.get_data_by_id(post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+    
+    if request.method == "POST":
+        title = request.form.get("title")
+        author = request.form.get("author")
+        content = request.form.get("content")
+        data = {
+            "id": post_id,
+            "title": title,
+            "author": author,
+            "content": content
+        }
+        provider.update_data(post_id, data)
+        return redirect(url_for("index"))
+    return render_template("update.html", post=post)
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
